1f5f7f4b194c77876dc421b8e549daa21709aa4a
Red Moses <musa.dhk@gmail.com>
2014-04-30 13:16:31 +0600

    started with the admin controller

---
+++ Tripodea.Web/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tripodea.Web.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
