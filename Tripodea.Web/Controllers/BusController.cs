817f29fee228ddc000b6ab1969103ece23b6339f
redmoses <musa@redmoses.org>
2014-05-02 09:20:13 +0600

    Test commit to MS

--- Tripodea.Web/Controllers/BusController.cs
+++ Tripodea.Web/Controllers/BusController.cs
@@ -13,9 +13,10 @@ namespace Tripodea.Web.Controllers
     public class BusController : Controller
     {
         private readonly IBusService _busService;
-
+        
         public BusController(IBusService busService)
         {
+            //get the bus service interface
             _busService = busService;
         }
 
