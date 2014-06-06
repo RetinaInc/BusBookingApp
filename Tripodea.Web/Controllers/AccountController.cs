7017b7e9f8e9da2848ddbac02c1c4185fea98ee9
Rabin Awal <morbid_leathal@hotmail.com>
2014-06-06 20:19:24 +0545

    Update AccountController.cs
    added first name , last name and email

--- Tripodea.Web/Controllers/AccountController.cs
+++ Tripodea.Web/Controllers/AccountController.cs
@@ -75,7 +75,12 @@ namespace Tripodea.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser() { UserName = model.UserName };
+                var user = new ApplicationUser() { 
+                    UserName = model.UserName 
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email
+                    };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
