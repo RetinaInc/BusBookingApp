630289e1a006d400356e156a6fe8d73251f791f3
Rabin Awal <morbid_leathal@hotmail.com>
2014-06-07 22:08:14 +0545

    Update IdentityModels.cs
    added first name , last name, email property

--- Tripodea.Web/Models/IdentityModels.cs
+++ Tripodea.Web/Models/IdentityModels.cs
@@ -5,6 +5,14 @@ namespace Tripodea.Web.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
